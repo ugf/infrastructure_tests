ccb49e9c9e9e4886b0eca4585917023a86315a70
mike suarez <mikemps@gmail.com>
2013-01-31 15:06:15 -0500

    371: testing powershell lib

--- step_definitions/powershell.rb
+++ step_definitions/powershell.rb
@@ -1,8 +1,8 @@
 Then /^Powershell has modules:$/ do |modules|
-  p modules_dir = "#{ENV['windir']}\\system32\\WindowsPowershell\\v1.0\\modules"
+  #modules_dir = "#{ENV['windir']}\\system32\\WindowsPowershell\\v1.0\\modules"
+  modules_dir = "/windows/system32/WindowsPowershell/v1.0/modules"
 
   modules.raw.each do |m|
-    p "#{modules_dir}\\#{m[0]}"
-    Dir.should exist "#{modules_dir}\\#{m[0]}"
+    Dir.should exist "#{modules_dir}/#{m[0]}"
   end
 end
