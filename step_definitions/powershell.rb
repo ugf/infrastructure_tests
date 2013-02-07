3630c0ad05e59d3c8f03bf47ac1a17ebe04dd6e5
msuarz <mikemps@gmail.com>
2013-02-07 09:53:55 -0500

    371: powershell library tests

--- step_definitions/powershell.rb
+++ step_definitions/powershell.rb
@@ -1,8 +1,8 @@
 Then /^Powershell has modules:$/ do |modules|
-  #modules_dir = "#{ENV['windir']}\\system32\\WindowsPowershell\\v1.0\\modules"
-  modules_dir = "/windows/system32/WindowsPowershell/v1.0/modules"
 
-  modules.raw.each do |m|
-    Dir.should exist "#{modules_dir}"
+  modules_dir = "/windows/sysnative/WindowsPowershell/v1.0/modules"
+
+  modules.raw.flatten.each do |m|
+    Dir.should exist "#{modules_dir}/#{m}"
   end
 end
