7ab9af08cb8faa119aa81ac8eecc266de8bce565
Isel Fernandez <isel_77@hotmail.com>
2013-02-18 14:23:44 -0500

    376 - adding Directory exists

--- step_definitions/file_system.rb
+++ step_definitions/file_system.rb
@@ -5,3 +5,7 @@ end
 Then /^Files exist:$/ do |files|
   files.raw.flatten.each { |file| File.should exist file}
 end
+
+Then /^Directory exists "([^"]*)"$/ do |directory|
+  Dir.should exist directory
+end
