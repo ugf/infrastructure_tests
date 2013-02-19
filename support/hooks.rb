ca9eead0d7882c1e338419432c63633c3ece4339
Isel Fernandez <isel_77@hotmail.com>
2013-02-19 09:51:26 -0500

    376 - improving route53

--- support/hooks.rb
+++ support/hooks.rb
@@ -1,5 +1,5 @@
 Before '@route53' do
-  if ENV['route53/domain'].nil? || ENV['route53/domain'].empty?
+  if ENV['route53/domain'].nil?
     puts 'should not use domain for route53'
     ip = ENV['route53/ip'].split '.'
     ENV['route53/prefix'] ||= ip[0]
